6b2a97419f4198a7a817d61ca17867c9e0e3ceba
3A632010 <a0921339421@gmail.com>
2019-10-08 21:17:30 +0800

    測試monolog

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require '../vendor/autoload.php';
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 $world = new World();
 $lara= new Lara();
@@ -11,3 +13,11 @@ $vincent= new Hello\Someone('Vincent');
 
 $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
